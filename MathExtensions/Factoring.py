bb742af1bc628b46902e74b726b6e1c69d1824bb
Quintkat <qlobster@me.com>
2021-11-03 01:58:06 +0100

    Divisors

--- MathExtensions/Factoring.py
+++ MathExtensions/Factoring.py
@@ -1,12 +1,20 @@
 from math import sqrt
 from typing import List
-import datetime
-import time
+from MathExtensions import Prime
 
-def primeFactors(n) -> List[int]:
+
+def primeFactors(n : int) -> List[int]:
 	factors = []
 	m = 1
 	n_ = n
+	prime = Prime.primeFirstResult(n)
+	if prime[0]:
+		return [n]
+	else:
+		n_ = n_//prime[1]
+		m *= prime[1]
+		factors.append(prime[1])
+
 	while n_ % 2 == 0:
 		n_ = n_//2
 		m *= 2
@@ -24,7 +32,7 @@ def primeFactors(n) -> List[int]:
 	return factors
 
 
-def primeFactorsUnique(n) -> List[int]:
+def primeFactorsUnique(n : int) -> List[int]:
 	allFactors = primeFactors(n)
 	uniqueFactors = []
 	for f in allFactors:
@@ -33,4 +41,53 @@ def primeFactorsUnique(n) -> List[int]:
 
 	return uniqueFactors
 
-print(primeFactors(34171))
+# Initial version of divisors, that is 10x slower than the new version below
+# def divisors(n : int) -> List[int]:
+# 	ds = [1]
+# 	for i in range(2, n//2 + 1):
+# 		if n % i == 0:
+# 			ds.append(i)
+#
+# 	ds.append(n)
+#
+# 	return ds
+
+
+def divisors(n : int, sort=True) -> List[int]:
+	factors = primeFactors(n)
+	multiplyList = [0]*len(factors)
+	ds = []
+
+	# Iterate over "Binary" list as if it's counting
+	finished = False
+	while not finished:
+		divisor = 1
+		# With current list, create a divisor
+		for i in range(len(multiplyList)):
+			if multiplyList[i] == 1:
+				divisor *= factors[i]
+		ds.append(divisor)
+
+		# Iterate
+		multiplyList[0] += 1
+		for i in range(len(multiplyList) - 1):
+			if multiplyList[i] == 2:
+				multiplyList[i + 1] += 1
+
+		# End condition
+		if multiplyList[-1] == 2:
+			finished = True
+
+		# Reduce integers back
+		for i in range(len(multiplyList)):
+			if multiplyList[i] == 2:
+				multiplyList[i] = 0
+
+	if sort:
+		return sorted(ds)
+	return ds
+
+
+# print(primeFactors(34171))
+# print(divisors(8162))
+# print(divisorsAlt(8162))
--- MathExtensions/Prime.py
+++ MathExtensions/Prime.py
@@ -1,4 +1,5 @@
 from math import sqrt
+from typing import Tuple
 
 
 def prime(n) -> bool:
@@ -9,6 +10,14 @@ def prime(n) -> bool:
 	return True
 
 
+def primeFirstResult(n) -> Tuple[bool, int]:
+	for i in range(2, int(sqrt(n)) + 1):
+		if n % i == 0:
+			return False, i
+
+	return True, 0
+
+
 # TODO: implement Miller-Rabin for primality testing, as this can be faster for very large n
-def MillerRabinTest(n):
+def MillerRabinTest(n, samples) -> bool:
 	pass
